6b5ca3bbea6cb679b0808d30e5d9617e2ff734db
NARA <97573525+nr1017@users.noreply.github.com>
2022-12-08 11:28:11 +0900

    Create Day7(각도기).js

---
+++ CodingTest/Day7(각도기).js
@@ -0,0 +1,22 @@
+/* 문제 설명
+각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다. 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+*/
+
+// 나의 풀이
+
+function solution(angle) {
+  return (angle < 90) ? 1
+  : (angle === 90) ? 2
+  : (angle < 180) ? 3
+  : (angle === 180) ? 4
+  : '';
+}
+
+// 다른 사람의 풀이
+
+function solution(angle) {
+    return [0, 90, 91, 180].filter(x => angle>=x).length;
+}
+
+//filter() 메서드는 array 관련 메서드로 조건에 맞는 요소들만 모아서 새로운 배열을 반환
+
